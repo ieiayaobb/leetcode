d907af238c54c8aa0c9c932c8488236654519378
Yineng.shen <yineng.shen@epicgames.com>
2019-08-23 17:30:27 +0800

    Solve 219, 225, 237

---
+++ 219.存在重复元素-ii.java
@@ -0,0 +1,11 @@
+/*
+ * @lc app=leetcode.cn id=219 lang=java
+ *
+ * [219] 存在重复元素 II
+ */
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        
+    }
+}
+
--- 225.用队列实现栈.java
+++ 225.用队列实现栈.java
@@ -1,34 +1,40 @@
+import java.util.concurrent.LinkedBlockingQueue;
+
 /*
  * @lc app=leetcode.cn id=225 lang=java
  *
  * [225] 用队列实现栈
  */
 class MyStack {
-
-    private Queue queue = new Queue();
-    /** Initialize your data structure here. */
+    private Queue<Integer> a;//输入队列
+    private Queue<Integer> b;//输出队列
+    
     public MyStack() {
-        
+        a = new LinkedList<>();
+        b = new LinkedList<>();
     }
     
-    /** Push element x onto stack. */
     public void push(int x) {
-        
+        a.offer(x);
+        // 将b队列中元素全部转给a队列
+        while(!b.isEmpty())
+            a.offer(b.poll());
+        // 交换a和b,使得a队列没有在push()的时候始终为空队列
+        Queue temp = a;
+        a = b;
+        b = temp;
     }
     
-    /** Removes the element on top of the stack and returns that element. */
     public int pop() {
-        
+        return b.poll();
     }
-    
-    /** Get the top element. */
+   
     public int top() {
-        
+        return b.peek();
     }
     
-    /** Returns whether the stack is empty. */
     public boolean empty() {
-        
+        return b.isEmpty();
     }
 }
 
